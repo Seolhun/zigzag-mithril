b081bd3a3ed9a3ecf1b64ad26fe6eafdd48b2feb
shun10114@gmail.com <shun10114@gmail.com>
2017-11-15 23:32:10 +0900

    XHR and Form, Routed example is completed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 // index.js
 var m = require("mithril")
 var MyComponent = require("./component/mycomponent")
+var UserList = require("./view/UserList")
+var UserForm = require("./view/UserForm")
 
+m.route(document.body, "/list", {
+	"/list": UserList,
+	"/edit/:id": UserForm,
+})
 
-m.mount(document.getElementById('sub-main'), MyComponent)
-m.render(document.getElementById("main"), "Hello Mithril.js")
+// m.mount(document.getElementById('user-list'), UserList)
+// m.mount(document.getElementById('sub-main'), MyComponent)
+// m.render(document.getElementById("main"), "Hello Mithril.js")
